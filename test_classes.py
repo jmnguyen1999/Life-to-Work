b3624f9e608af5d5c83846db98c861e7efcb908e
jmnguyen1999 <jmnguyen1999@gmail.com>
2022-10-04 01:27:17 +0000

    Finished testing all methods of Day class EXCEPT choose_random_time(), could try more combinations of methods as well

--- test_classes.py
+++ test_classes.py
@@ -293,9 +293,6 @@ class ShiftType:
         self.limit_day = limit_day      # max # of hrs that can worked/day on this type of work
         self.note = note                #optional note for user's personal use
 
-        self.schedule = []              #utilized to save each Shift object saved on each day
-                                        # --> list of size 7 for 7 days --> e.g. index 0 == Shift object scheduled for this ShiftType on Day 1
-
 
 #Issue: can't save multiple Shift objects in shifts for "Unavailability" key --> yes an unavailability is necessary
 #^ issue for "work" too, --> cannot use as keys anymore, maybe no more dictionary
@@ -307,12 +304,39 @@ class ShiftType:
 #               --> also won't need unavailable_times for good anymore.
 
 #   - wrong calculation for shift_hrs, forgot to think about is_work field
+#Issue #2:      Unavailability shouldn't be counted towards "Shifts" that aren't Work! Unavailability only accounted for work Shifts, but if I'm inputting a random event thats not work --> should be able to input no matter the unavailability
+#       --> Shifts are allowed to intersect IFFFFF they are NOT work Shifts
+#       --> Shifts trying to be scheduled that == Work cannot intersect ANYTHING, cannot intersect ever
+#       --> Shifts trying to be schedule that != Work can intersect Unavailability Shifts and notthinng else
+#                   why? --> bc off time events & unavailability are basically the same thing
+#                   --> Generally, all Shift objects that is_work = False --> can intersect
+#           How to print/display:
+#               - show the event for the whole scheduled time no matter what --> whatever Unavailability cuts around it:
+#                   e.g. unavail = 09:00pm - 12:00am, event = 11:00 - 12:00am
+#                           unavail = 09:00pm - 11:00pm , event = 11:00pm - 12:00am,
+#           How to schedule:
+#               - if shift.is_work = False:
+#                   - additional_avail_times = all unavail times
+#                   - shift has valid times if within avail_range and/or unavail_times
+#                       --> if uses unavail_times:
+#                               - Take out what can from avail_range
+#                               - Take out the the times from unavail_times
+#                           #Question:  readjust the Unavailability Shifts in shifts? might not be needed --> just leave the shifts intersecting and leave it up to display's problem
+#           How to remove from schedule:
+#               - if shift.is_work = False:
+#                   - give/re-create the avail times for avail_times
+#                   - if the times intersect with any of unavailable Shift times --> re-create to make them avail to off work Shifts (but DO NOT re-include in the avail_times, bc its not avail to everyone)
+#           Solution:
+#               - need an avail_times for on_work and off_work shifts:
+#                   --> change avail_times -->
+#                   --> create off_work_avail_times     --> avail times specific to events that aren't for work:
+#                               --> TODO: for later, maybe change Shift class --> TimePeriod class, then create new Shift class that uses TimePeriod, and create new Event class that uses TimePeriod --> able to specify when things are an actual Shift (always for work), and when an Event (always for non-work)
 #Note for self:    do we need "unavailability"/"timeoff" as a Shift obj or can we just include the timeframe in the unavailable? --> i included as a Shift obj originally to also include labels when printing the schedule
 class Day:
 
     #sched_shifts = List of Shift objs representing already scheduled things this week
     #day_dt = datetime obj that this Day obj is supposed to represent e.g. 10/20/22
-    def __init__(self, label, sched_shifts, limit_hrs, shift_buffer):
+    def __init__(self, label, sched_shifts, unavailability, limit_hrs, shift_buffer):
                                         #purpose --> easy access to see times of this Day that are occupied with a shift
         self.shifts = {}                #Dictionary of ALL Shift objects that are scheduled for this Day: key = starttime_str (datetime str: %I:%M%p),  value = Shift obj
         self.shifttypes = {}             #dictionary of the Shift objects that represent different WorkTypes --> used for easy access to adjust times during scheduling: key = WorkType.label, value = Shift obj
@@ -323,27 +347,69 @@ class Day:
         self.limit_hrs = limit_hrs      # max # of hrs that can worked this day
         self.shift_buffer = shift_buffer    # min. amount of time in minutes allowed between consecutive Shifts, e.g. buffer = 30 min, shift 1 = 8:00am --> shift 2 must be >= 8:30am
 
-        self.avail_range = self.init_avail_range()
 
-        #Populate shifts and unavailable_times:
+        self.unavailable = unavailability           #list of tuples, used to JUST save and reference the Day's unavailability for is_work = True Shifts! --> used in scheduling to determine what hrs are available to different Shifts (is_work = True/False)
+
+
+        self.avail_range = {}
+        self.nonwork_avail_range = {}   #same as avail_range, but EXCLUSIVELY avail only to Shifts who's is_work = False --> able to schedule non-work events during unavailability, initialized to all unavailable times
+
+        self.init_avail_range()
+        #Populate unavailable:
+        #for shift in sched_shifts:
+        #    if shift.label == "Unavailability":
+        #        self.unavailable.append((shift.start_time, shift.end_time))
+
+        #Initialize nonwork_avail_range now that Day.unavailable is initialized:
+        #for tuple in self.unavailable:
+        #    start_time = tuple[0]
+        #    end_time = tuple[1]
+
+        #     time_counter = start_time
+        #    while time_counter <= end_time:
+        #        self.nonwork_avail_range[time_counter.strftime('%I:%M%p')] = True
+        #        time_counter = time_counter + timedelta(minutes=self.shift_buffer)       #add buffer minutes to it
+
+        #Populate the shifts now that nonwork_avail_range is also init with avail_range:
         for shift in sched_shifts:
-            self.sched_shift(shift)
+              self.sched_shift(shift)
 
 
     #Purpose: initializes self.avail_range with all possible times of the day (12am - 12am) using the given shift_buffer:
     def init_avail_range(self):
-        avail_range = {}       #keys = string with "00:00am/pm"    values = boolean if avail
 
         start_time_str = self.label + " 12:00am"
         time_counter = datetime.strptime(start_time_str, '%m/%d/%y %I:%M%p')
         next_day = datetime.strptime(self.label, '%m/%d/%y') + timedelta(days=1)
 
         while time_counter < next_day:         #while time_counter hasn't met with or surpassed onto the next day
-            avail_range[time_counter.strftime('%I:%M%p')] = True
+            if self.intersects_unavailable(time_counter):
+                self.nonwork_avail_range[time_counter.strftime('%I:%M%p')] = True
+
+            else:
+                self.avail_range[time_counter.strftime('%I:%M%p')] = True
             time_counter = time_counter + timedelta(minutes=self.shift_buffer)       #add buffer minutes to it
 
-        return avail_range
 
+
+    def intersects_unavailable(self, time):
+        for tuple in self.unavailable:
+            start_time = tuple[0]
+            end_time = tuple[1]
+            if time >= start_time and time <= end_time:
+                return True
+
+        return False
+        '''
+        unavail = 02:00pm - 04:00pm,  05:00pm - 07:00pm
+        event = 04:00pm - 06:00pm
+
+        nonwork: 02:00pm, 02:30pm, 03:00pm, 03:30pm, 04:00pm, 05:00pm, 05:30pm, 06:00pm, 06:30pm, 07:00pm
+        avail_range: 04:30pm, ...
+
+
+
+        '''
 
     def sched_shift(self, shift):
         #add shift to the shifts field, assuming all added shifts are already validated --> there will not be another shift with the same start_time
@@ -359,15 +425,20 @@ class Day:
                 self.shifttypes[shift.label] = [shift]
 
 
-        #adjust avail_range to each shift:
+        #adjust avail_range/nonwork_avail_range to each shift:
+        intersects = (shift.is_work is False) and (self.intersects_unavailable(shift.start_time) or self.intersects_unavailable(shift.end_time))
+
         #assuming Shift will always have exact times according shift_buffer:
         time_counter = shift.start_time
-        avail_times = self.avail_range.keys()
 
         while time_counter <= shift.end_time:       #deletes all avail times from start_time --> end_time
             time_counter_str = time_counter.strftime('%I:%M%p')
-            if time_counter_str in avail_times:
+
+            if time_counter_str in self.avail_range:
                 del self.avail_range[time_counter.strftime('%I:%M%p')]
+            if intersects and (time_counter_str in self.nonwork_avail_range):
+                del self.nonwork_avail_range[time_counter.strftime('%I:%M%p')]
+
             time_counter = time_counter + timedelta(minutes=self.shift_buffer)
 
         #update total amount of shift_hrs:
@@ -384,6 +455,8 @@ class Day:
 
 
     #no error handling done --> TODO
+    #Issue, now that unavailability and nonwork can intersect --> it overrides the Shift value at the same key (starttime), --> make value = List of Shifts??
+    #       --> at this point might as well just not save Unavailability as a Shift obj anymore
     def remove_shift(self, starttime_str):
         #Check if shift even exists;
         if starttime_str in self.shifts:
@@ -394,10 +467,17 @@ class Day:
                 del self.shifttypes[del_shift.label]
 
             #assuming Shift will always have exact times according shift_buffer:
+            intersects = (del_shift.is_work is False) and (self.intersects_unavailable(del_shift.start_time) or self.intersects_unavailable(del_shift.end_time))
+
             time_counter = del_shift.start_time
 
             while time_counter <= del_shift.end_time:       #create all consecutive avail times from start_time --> end_time
-                self.avail_range[time_counter.strftime('%I:%M%p')] = True
+                if intersects and self.intersects_unavailable(time_counter):
+                    self.nonwork_avail_range[time_counter.strftime('%I:%M%p')] = True
+
+                else:
+                    self.avail_range[time_counter.strftime('%I:%M%p')] = True
+
                 time_counter = time_counter + timedelta(minutes=self.shift_buffer)
 
             if del_shift.is_work:
@@ -411,9 +491,13 @@ class Day:
                 self.limit_hrs_hit = False
 
     #Purpose: Works with choose_random_time(), validates whether a given duration from start_time to end_time is avail in self.avail_range
-    def is_avail(self, start_time, end_time):
+    def is_avail(self, is_work, start_time, end_time):
         time_counter = start_time
         all_times = self.avail_range.keys()
+
+        #If shift is non-work --> able to schedule during nonworking hours (includes unavailability times):
+        if is_work is False:
+            all_times.append(self.nonwork_avail_range.keys())
 
         #just check if each time string exists as a key in the avail_range list of keys, if not --> there must be some time thats needed that is unavail
         while time_counter <= end_time:
@@ -447,7 +531,8 @@ class Day:
 
 class Week:
     #sched_shifts = dictionary of Shift objects representing already scheduled things this week (keys = datetime label to use for new Day objects, value = List of Shift objects for that Day)
-    def __init__(self, sched_shifts, shifttypes_req, limit_hrs_day = 24, shift_buffer = 30):
+    #unavailability = dictionary : keys = day.label (date str), value = List of tuples (tuples = (start_time, end_time), representing one unavailability of that particular Day
+    def __init__(self, sched_shifts, shifttypes_req, unavailability, limit_hrs_day = 24, shift_buffer = 30):
         self.days = {}                              #dictionary of size 7 to represent 7 Days --> keys = Day.label ,  value = Day obj
         self.shifttypes_req = shifttypes_req        #dictionary of all ShiftType objects needed to be scheduled throughout self.days --> keys = ShiftType.label  ,  value = ShiftType obj
         self.total_hrs_req = 0                      # total amount of hrs to schedule through Week.days  --> == sum of hrs_req in Week.shifttypes_req
@@ -459,7 +544,8 @@ class Week:
 #Note to self: Could also just do this from the getgo when parsing input --> pass as parameter already formed in Week constructor
         for daylabel in list(sched_shifts.keys()):
             shifts_day = sched_shifts.get(daylabel)
-            self.days[daylabel] = Day(daylabel, shifts_day, self.limit_hrs_day, self.shift_buffer)
+            unavailable_day = unavailability.get(daylabel)
+            self.days[daylabel] = Day(daylabel, shifts_day, unavailable_day, self.limit_hrs_day, self.shift_buffer)
 
 
         #TODO: update total_hrs_req by summing all hrs_req in shifttypes_req
